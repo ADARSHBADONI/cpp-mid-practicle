2f170047c7f783d6f96cb9ae70508c0fd5436c6e
Theyellowflashminato <91362584+Theyellowflashminato@users.noreply.github.com>
2021-11-25 20:56:56 +0530

    Create q1.c

---
+++ q1.c
@@ -0,0 +1,25 @@
+//Q1.Write a C++ program to calculate the size of integer, unsigned integer, signed integer, character, signed char, unsigned char, float, double.
+#include<iostream>
+using namespace std;
+int main()
+{
+int integer;
+unsigned int uinteger;
+signed sinteger;
+char character;
+signed char scharacter;
+unsigned char ucharacter;
+float floatType;
+double doubleType;
+
+cout<<"NAME-ADARSH BADONI   SEC-G   STUDENT ID 20011501"<<endl;
+cout<<"size of integer is: "<<sizeof(integer)<<endl;
+cout<<"size of unsigned integer is: "<<sizeof(uinteger)<<endl;
+cout<<"size of signed integer is: "<<sizeof(sinteger)<<endl;
+cout<<"size of character is: "<<sizeof(character)<<endl;
+cout<<"size of signed character is: "<<sizeof(scharacter)<<endl;
+cout<<"size of unsigned character is: "<<sizeof(ucharacter)<<endl;
+cout<<"size of float is: "<<sizeof(floatType)<<endl;
+cout<<"size of double is: "<<sizeof(doubleType)<<endl;
+return 0;
+}
